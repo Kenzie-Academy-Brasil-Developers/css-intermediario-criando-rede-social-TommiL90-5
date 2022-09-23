0b18cbefc6fcc1d8a36aee0b53dc06f22bb8f2c2
TommiL90 <tomasbenjamin.l90@gmail.com>
2022-09-23 10:47:12 -0300

    avance 1

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,1 +1,15 @@
 /* Desenvolva a lógica da página aqui */
+
+const buttonsControllersModal = document.querySelectorAll("[data-control-modal]")
+
+
+for(let i = 0; i < buttonsControllersModal.length; i++){
+    
+    buttonsControllersModal[i].addEventListener("click", (event) =>{
+        event.preventDefault();
+        
+        let modalId = buttonsControllersModal[i].getAttribute("data-control-modal")
+
+        document.getElementById(modalId).classList.toggle("show-modal")
+    })
+}
